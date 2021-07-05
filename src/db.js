a13ed6f5f798f5989c2d4423492c5524a5841c67
juuunobae <juuunobae@gmail.com>
2021-07-05 22:42:47 +0900

    코드에 주석달기

--- src/db.js
+++ src/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+// mongDB와 연결
+// 터미널에 mongo를 입력했을 때 나오는 url과 /database 이름
 mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -7,8 +9,12 @@ mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
 
 const db = mongoose.connection;
 
+// 이벤트 발생 시 콜백함수
 const handleError = (error) => console.log("❌DB Error", error);
 const handleOpen = () => console.log("✅ Connected DB");
 
+// database 연결 시 에러가 발생하면 실행될 이벤트
 db.on("error", handleError);
+
+// database 연결 성공 시 실행될 이벤트
 db.once("open", handleOpen);
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,16 +1,20 @@
 import mongoose from "mongoose";
 
+// 데이터의 스키마 작성
 const videoSchema = new mongoose.Schema({
-  title: String,
+  title: String, // model의 데이터 type을 정의해준다.
   description: String,
   createAt: Date,
-  hashtags: [{ type: String }],
+  hashtags: [{ type: String }], // 배열도 가능하다.
   meta: {
+    // 객체도 가능하다.
     views: Number,
     rating: Number,
   },
 });
 
+// 작성한 스키마를 토대로 모델 생성, (모델이름, 설정한 스키마)
 const Video = mongoose.model("Video", videoSchema);
 
+// 모델 export
 export default Video;
